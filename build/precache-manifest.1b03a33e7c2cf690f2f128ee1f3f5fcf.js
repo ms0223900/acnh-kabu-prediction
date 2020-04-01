5a9a62f55687552aaba80114517f75f63cb00609
ms0223900 <ms0223900@gmail.com>
2020-04-01 16:19:57 +0800

    build: update build

---
+++ build/precache-manifest.1b03a33e7c2cf690f2f128ee1f3f5fcf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "257c1db5509d2fc8f982c2f0ea332aa9",
+    "url": "/acnh-kabu-prediction/index.html"
+  },
+  {
+    "revision": "55dc52e488638cc427ca",
+    "url": "/acnh-kabu-prediction/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "d37f3f1b8e46fbe87542",
+    "url": "/acnh-kabu-prediction/static/js/2.bc5fa383.chunk.js"
+  },
+  {
+    "revision": "55dc52e488638cc427ca",
+    "url": "/acnh-kabu-prediction/static/js/main.3af3b228.chunk.js"
+  },
+  {
+    "revision": "3b77ed4930544b0fe8e6",
+    "url": "/acnh-kabu-prediction/static/js/runtime-main.4096e7c4.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e89eb9dac8d3477d24280404383c969c.js"
+  "/acnh-kabu-prediction/precache-manifest.1b03a33e7c2cf690f2f128ee1f3f5fcf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/acnh-kabu-prediction/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
